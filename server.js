066f405edf436b16f68dbf86e20207f534fbbfce
GabrielTMangum <gabrieltmangum@gmail.com>
2019-11-20 13:46:31 -0500

    pushing finalized file structure as well server.js

--- server.js
+++ server.js
@@ -1,25 +1,26 @@
 const express = require("express");
 const path = require("path");
 const fs = require('fs')
-const reservations = require('../storage/reservations.json')
-const waitlist = require('../storage/waitlist.json')
+const reservations = require('./public/assets/storage/reservations.json')
+const waitlist = require('./public/assets/storage/waitlist.json')
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+app.use(express.static('public'))
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 app.get('/', function (req, res) {
-    res.sendFile(path.join(__dirname, "../../index.html"));
+    res.sendFile(path.join(__dirname, "/public/index.html"));
 });
 
 app.get('/reserve', function (req, res) {
-    res.sendFile(path.join(__dirname, "../../make.html"));
+    res.sendFile(path.join(__dirname, "/public/make.html"));
 });
 
 app.get('/tables', function (req, res) {
-    res.sendFile(path.join(__dirname, "../../view.html"));
+    res.sendFile(path.join(__dirname, "/public/view.html"));
  });
 
 app.get('/api/tables', (req, res) => {
@@ -50,7 +51,7 @@ app.post('/api/tables', (req, res) => {
     }
     res.json(req.body)
     
-        fs.writeFile(`../storage/${listKey}.json`, JSON.stringify(list), function(err) {
+        fs.writeFile(`/public/assets/storage/${listKey}.json`, JSON.stringify(list), function(err) {
             
             if(err) throw err;
 
